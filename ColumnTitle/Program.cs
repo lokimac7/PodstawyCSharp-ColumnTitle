12089b7eb5b696306c5fc1548afba6cae6b3f703
MACIEJ CZERWINSKI <MACIEJ.CZERWINSKI@BEAUTYOFSCIENCE.PL>
2021-07-08 11:47:50 +0200

    ColumnTitle_v1

---
+++ ColumnTitle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ColumnTitle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ColumnTitle ct = new ColumnTitle();
+            Console.WriteLine(ct.TitleToNumber("WAAAB"));
+            Console.WriteLine(ct.TitleToNumber("AAAB"));
+            Console.WriteLine(ct.TitleToNumber("ZZZZ"));
+            Console.WriteLine(ct.TitleToNumber("ABCD"));
+            Console.WriteLine(ct.TitleToNumber("KLASA"));
+            Console.WriteLine(ct.TitleToNumber("wsd"));
+            Console.ReadKey();
+        }
+    }
+
+    public class ColumnTitle
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int result = 0;
+
+            foreach (var item in columnTitle.ToUpper())
+            {
+                int index = alphabet.IndexOf(item) + 1;
+                result = result * 26 + index;
+            }
+            return result;
+        }
+    }
+}
